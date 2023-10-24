42a0590ed1723777e3ea67ef209352ca81b7402a
HenryCodeT <120686@unsaac.edu.pe>
2023-10-23 20:35:59 -0500

    first commit

---
+++ src/main/java/com/rest/webservices01/restfullwebservices/RestfullWebServices01Application.java
@@ -0,0 +1,13 @@
+package com.rest.webservices01.restfullwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfullWebServices01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfullWebServices01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/rest/webservices01/restfullwebservices/RestfullWebServices01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.webservices01.restfullwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfullWebServices01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
